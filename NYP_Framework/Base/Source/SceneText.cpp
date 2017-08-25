eeb46e8b54f9da4991040376222bba6dd1744b6b
Shih Wei <loneswei@gmail.com>
2017-08-25 11:42:41 +0800

    Changed obj file for equipment(Download it from google drive under art models folder(under equipment folder))

--- NYP_Framework/Base/Source/SceneText.cpp
+++ NYP_Framework/Base/Source/SceneText.cpp
@@ -184,12 +184,12 @@ void SceneText::Init()
 	MeshBuilder::GetInstance()->GenerateOBJ("floor", "OBJ//cube.obj"); //remember to change texture
 
 	//Equipment Meshes
-	MeshBuilder::GetInstance()->GenerateOBJ("Turret", "OBJ//cube.obj"); //remember to change obj
-	MeshBuilder::GetInstance()->GenerateOBJ("Healing Station", "OBJ//cube.obj");
+	MeshBuilder::GetInstance()->GenerateOBJ("Turret", "OBJ//equipmentCube.obj"); //remember to change obj
+	MeshBuilder::GetInstance()->GenerateOBJ("Healing Station", "OBJ//equipmentCube.obj");
 	MeshList::GetInstance()->GetMesh("Healing Station")->textureID[0] = LoadTGA("Image//Equipment//Heal_Active.tga");
-	MeshBuilder::GetInstance()->GenerateOBJ("Floor Spike", "OBJ//cube.obj");
+	MeshBuilder::GetInstance()->GenerateOBJ("Floor Spike", "OBJ//equipmentCube.obj");
 	MeshList::GetInstance()->GetMesh("Floor Spike")->textureID[0] = LoadTGA("Image//Equipment//Spike.tga");
-	MeshBuilder::GetInstance()->GenerateOBJ("Shield", "OBJ//cube.obj"); //remember to change obj
+	MeshBuilder::GetInstance()->GenerateOBJ("Shield", "OBJ//equipmentCube.obj"); //remember to change obj
 
 	
 	//Shop
